089bb5b37e59ed59c3ba886eca7d39b9e5e21e05
rohanbabbar <rohanbabbar0408@gmail.com>
2022-01-28 23:23:28 +0530

    Added the game algo

--- app/src/main/java/com/example/chainreactiongameapp/GameActivity.java
+++ app/src/main/java/com/example/chainreactiongameapp/GameActivity.java
@@ -46,26 +46,70 @@ public class GameActivity extends AppCompatActivity {
         adapter.setOnItemClickListener(new GameAdapter.onItemClickListener() {
             @Override
             public void onClickListener(int position) {
-                if (pos_me==players.size()) {
+                if (pos_me>=players.size()) {
                     pos_me=0;
                 }
                 int xPos = position/8;
                 int yPos = position%8;
                 String player_name = players.get(pos_me).getName();
-                if (game_array[xPos][yPos].equals("Player") && game_max_values[xPos][yPos]>=1){
-                    game_array[xPos][yPos] = "one" + player_name;
-                    pos_me++;
-                } else if (game_array[xPos][yPos].contains("one") && game_array[xPos][yPos].contains(player_name) && game_max_values[xPos][yPos]>=2) {
-                    game_array[xPos][yPos] = "two" + player_name;
-                    pos_me++;
-                }else if (game_array[xPos][yPos].contains("two") && game_array[xPos][yPos].contains(player_name) && game_max_values[xPos][yPos]>=3) {
-                    game_array[xPos][yPos] = "three" + player_name;
-                    pos_me++;
+                if (chain_break(xPos,yPos,player_name)) {
+                    game_array[xPos][yPos] = "Player";
+                }else {
+                    move(xPos, yPos, player_name);
                 }
-                Log.d("message",Arrays.deepToString(game_array));
                 adapter.setGame_array(game_array,position);
             }
         });
         recyclerView.setAdapter(adapter);
+    }
+
+
+    public boolean chain_break(int i,int j,String name) {
+        if (game_max_values[i][j]==1 && game_array[i][j].contains("one") || game_max_values[i][j]==2 &&
+                game_array[i][j].contains("two") || game_max_values[i][j]==3 &&
+                game_array[i][j].contains("three")) {
+            if (i-1>=0) {
+                set_condition(i-1,j,name);
+                move(i - 1, j, name);
+            }
+            if (j-1>=0) {
+                set_condition(i,j-1,name);
+                move(i, j - 1, name);
+            }
+            if (i+1<game_array.length) {
+                set_condition(i+1,j,name);
+                move(i+1,j,name);
+            }
+            if (j+1<game_array[0].length) {
+                set_condition(i,j+1,name);
+                move(i,j+1,name);
+            }
+            pos_me--;
+            return true;
+        }
+        return false;
+    }
+
+    public void move(int xPos,int yPos,String player_name) {
+        if (game_array[xPos][yPos].equals("Player") && game_max_values[xPos][yPos]>=1){
+            game_array[xPos][yPos] = "one" + player_name;
+            pos_me++;
+        } else if (game_array[xPos][yPos].contains("one") && game_array[xPos][yPos].contains(player_name) && game_max_values[xPos][yPos]>=2) {
+            game_array[xPos][yPos] = "two" + player_name;
+            pos_me++;
+        }else if (game_array[xPos][yPos].contains("two") && game_array[xPos][yPos].contains(player_name) && game_max_values[xPos][yPos]>=3) {
+            game_array[xPos][yPos] = "three" + player_name;
+            pos_me++;
+        }
+    }
+
+    public void set_condition(int i,int j,String name) {
+        if (game_array[i][j].contains("one") && !game_array[i][j].contains(name)) {
+            game_array[i][j] = game_array[i][j].substring(0,3) + name;
+        }else if (game_array[i][j].contains("two") && !game_array[i][j].contains(name)) {
+            game_array[i][j] = game_array[i][j].substring(0,3) + name;
+        }else if (game_array[i][j].contains("three") && !game_array[i][j].contains(name)) {
+            game_array[i][j] = game_array[i][j].substring(0,6) + name;
+        }
     }
 }
