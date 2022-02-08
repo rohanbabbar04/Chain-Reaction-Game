45eb451d522752ecd654b522b6362b07184ffe1f
smartrajan <smartrajanmuz@gmail.com>
2022-02-08 22:45:22 +0530

    Added a new page splash screen

---
+++ app/src/main/java/com/example/chainreactiongameapp/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.chainreactiongameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMER=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        ImageView img = (ImageView)findViewById(R.id.imageView);
+        Animation aniRotate = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_clockwise);
+        img.startAnimation(aniRotate);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIMER);
+    }
+}
